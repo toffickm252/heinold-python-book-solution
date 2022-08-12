dfd3c48b5701de15e3141455a644fa8354edba33
Toffick Mohammed <toffickm252@gmail.com>
2022-08-12 07:48:05 +0000

    start

---
+++ chapter_one/que_6.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 12 07:39:33 2022
+
+@author: TOFFICK
+"""
+
+number=int(input("enter the number "))
+
+print("The square of "+str(number)+" is "+str(number*number))
